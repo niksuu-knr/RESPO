1ce2bf8813ba7cd4dff755fed44ef097e0d32782
niksuu-knr <ntuoreniemi1@gmail.com>
2026-05-22 00:47:09 +0300

    Update interface.js

--- src/interface.js
+++ src/interface.js
@@ -1,16 +1,39 @@
 /**
- * UI: Hallintapaneeli (PRG-Tila)
+ * UI: MASTER INTERFACE
+ * Piirtää näppäimistön ja yhdistää logiikan
  */
 const UI = {
-    // Ohjelmointi-funktio, jota kutsutaan PRG-tilassa
+    initKeyboard: () => {
+        const kb = document.getElementById('keyboard');
+        kb.innerHTML = ''; // Tyhjennä vanhat
+        
+        // Luodaan painikkeet (esimerkki)
+        const keys = ["KAHVI", "PULLA", "PRG-EDIT", "CASH"];
+        keys.forEach(k => {
+            let b = document.createElement('div');
+            b.className = 'key';
+            b.innerText = k;
+            b.onclick = () => UI.handleButton(k);
+            kb.appendChild(b);
+        });
+    },
+
+    handleButton: (action) => {
+        if (action === "PRG-EDIT") {
+            UI.enterProgrammingMode();
+        } else {
+            console.log("Käsitellään myynti: " + action);
+        }
+    },
+
     enterProgrammingMode: () => {
         const id = prompt("Syötä tuotenumero:");
         const name = prompt("Syötä tuotteen nimi:");
         const price = prompt("Syötä hinta:");
         
         if(id && name && price) {
-            Kernel.programPLU(id, name, parseFloat(price));
-            alert("Tuote " + name + " tallennettu kassan muistiin!");
+            SPS2000_Kernel.programPLU(id, name, parseFloat(price));
+            alert("Tuote " + name + " tallennettu pysyvästi!");
         }
     }
 };
