4fc2d7858c65f75f804b0c84882f5436345f317b
niksuu-knr <ntuoreniemi1@gmail.com>
2026-05-22 01:06:31 +0300

    Update interface.js

--- src/interface.js
+++ src/interface.js
@@ -1,25 +1,24 @@
 const UI = {
     initKeyboard: function() {
-        console.log("UI: Piirretään näppäimistöä...");
         const kb = document.getElementById('keyboard');
-        if (!kb) {
-            console.error("Virhe: #keyboard elementtiä ei löydy!");
-            return;
-        }
+        kb.innerHTML = '';
         
-        kb.innerHTML = ''; // Tyhjennä
-        const buttons = ["KAHVI", "PULLA", "PRG-EDIT", "CASH", "MODE"];
-        
-        buttons.forEach(btnText => {
+        for(let i = 1; i <= 40; i++) {
             let b = document.createElement('div');
-            b.className = 'key';
-            b.innerText = btnText;
-            b.onclick = () => {
-                if(btnText === "PRG-EDIT") UI.enterProgrammingMode();
-                else console.log(btnText + " painettu");
-            };
+            b.id = "key_" + i;
+            
+            // Määritellään värikoodit paikan mukaan (SPS-2000 logiikka)
+            if (i === 40) b.className = "key key-mod";     // MODE
+            else if (i >= 32 && i <= 39) b.className = "key key-func"; // CASH/VOID
+            else b.className = "key key-plu";              // Tuotteet
+            
+            b.innerText = "P" + i;
+            b.onclick = () => UI.processAction(i);
             kb.appendChild(b);
-        });
+        }
+    },
+    // ... loput funktiot samat kuin aiemmin ...
+};
         console.log("UI: Näppäimistö piirretty.");
     },
 
